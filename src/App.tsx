3c0a5d802809409fc1fd66b88e04ddf8efd3d8c1
alade lekan <aladelekan187@gmail.com>
2024-08-23 19:47:50 +0100

    fix: update ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 import { useState, useEffect } from "react";
-import "./App.css";
+import IconButton from "@mui/material/IconButton";
+import AddIcon from "@mui/icons-material/Add";
+import CheckIcon from "@mui/icons-material/Check";
+import SendIcon from "@mui/icons-material/Send";
+import {
+  Box,
+  Dialog,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  Stack,
+  Button,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Fab from "@mui/material/Fab";
 
 interface TodoType {
   id: string;
@@ -9,6 +25,7 @@ interface TodoType {
 
 function App() {
   const [title, setTitle] = useState<string>("");
+  const [open, setOpen] = useState<boolean>(false);
   const [isFetchTodo, setIsFetchTodo] = useState(true);
   const [todos, setTodos] = useState<TodoType[]>([]);
 
@@ -47,29 +64,74 @@ function App() {
   };
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div>
-          <input
-            name="title"
-            placeholder="Enter title"
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </div>
-        <button>Add Task</button>
-      </form>
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "80vh",
+        }}
+      >
+        <Stack width={500} border="1px solid #f1f1f1" borderRadius={1}>
+          <List disablePadding>
+            {todos.map((item) => (
+              <>
+                <ListItem>
+                  <ListItemText
+                    primary={
+                      <Typography fontWeight={600}> {item.title}</Typography>
+                    }
+                  />
+                  <IconButton
+                    aria-label="delete"
+                    onClick={() => removeHandler(item.id)}
+                    color="success"
+                  >
+                    <CheckIcon />
+                  </IconButton>
+                </ListItem>
+                <Divider />
+              </>
+            ))}
+          </List>
+        </Stack>
+      </Box>
+      <Stack direction="row" justifyContent="flex-end">
+        <Fab color="primary" aria-label="add" onClick={() => setOpen(true)}>
+          <AddIcon />
+        </Fab>
+      </Stack>
+      <Dialog onClose={() => setOpen(false)} open={open}>
+        <Stack bgcolor="white" p={3}>
+          <Typography fontWeight={600} mb={2} textAlign="center">
+            Create New Todo
+          </Typography>
+          <form onSubmit={onSubmit}>
+            <Stack direction="row" gap={2}>
+              <TextField
+                name="title"
+                size="small"
+                placeholder="Enter title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                sx={{ width: 300 }}
+              />
 
-      <div>
-        {todos.map((item) => (
-          <li>
-            {item.title}{" "}
-            <button onClick={() => removeHandler(item.id)}>
-              Mark as complete
-            </button>
-          </li>
-        ))}
-      </div>
-    </div>
+              <Button
+                type="submit"
+                size="small"
+                variant="outlined"
+                endIcon={<SendIcon />}
+              >
+                Send
+              </Button>
+            </Stack>
+          </form>
+        </Stack>
+      </Dialog>
+    </>
   );
 }
 
